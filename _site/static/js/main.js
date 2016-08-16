2a9b5bb8044fa507b2904e87bc17e46511ce83b3
fxcosta <fx3costa@gmail.com>
2016-08-15 23:07:02 -0300

    animate.css added

--- _site/static/js/main.js
+++ _site/static/js/main.js
@@ -3,12 +3,26 @@
  */
 // jQuery for page scrolling feature
 jQuery(document).ready(function(e) {
-    e(".scroll").click(function(t) {
-        t.preventDefault();
-        e("html,body").animate({
-            scrollTop: e(this.hash).offset().top
-        }, 1e3)
-    })
+	e(".scroll").click(function(t) {
+		t.preventDefault();
+		e("html,body").animate({
+			scrollTop: e(this.hash).offset().top
+		}, 1e3)
+	})
+});
+
+$.fn.extend({
+	animateCss: function (animationName) {
+		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		$(this).addClass('animated ' + animationName).one(animationEnd, function() {
+			$(this).removeClass('animated ' + animationName);
+		});
+	}
+});
+
+$("#avatar-container > img").hover(function(e) {
+	$('#avatar-container').removeClass('fadeInDown');
+	$('#avatar-container').toggleClass('infinite pulse');
 });
 
 
